9cb8ad13cb0a2298de1ea5363f290fb9e0e3ed10
D1-3105 <lyerhd@gmail.com>
2024-12-03 22:42:38 +0300

    fix launching

--- flymy_comfyui_repo_gen/scripts/main.py
+++ flymy_comfyui_repo_gen/scripts/main.py
@@ -1,3 +1,13 @@
+import os
+import pathlib
+import sys
+
+script_path = pathlib.Path(__file__).resolve()
+script_parent_dir = script_path.parent.parent.parent
+os.environ["PYTHONPATH"] = os.environ.get("PYTHONPATH", "") + ":" + str(script_parent_dir)
+sys.path.append(str(script_parent_dir))
+
+
 from flymy_comfyui_repo_gen.scripts.repo_generator import app
 
 if __name__ == "__main__":
