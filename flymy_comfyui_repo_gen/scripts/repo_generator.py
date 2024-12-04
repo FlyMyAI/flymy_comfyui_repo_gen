73a96bd26bc53ed725cc05566df1fac2260fa245
teith <teithant@gmail.com>
2024-12-04 20:33:57 +0100

    ux fixes

--- flymy_comfyui_repo_gen/scripts/repo_generator.py
+++ flymy_comfyui_repo_gen/scripts/repo_generator.py
@@ -11,49 +11,32 @@ from flymy_comfyui_repo_gen.core.generate_resolution import (
     generate_resolution_repositories,
 )
 from flymy_comfyui_repo_gen.schemas.RepoGeneratorConfig import (
-    RepoGeneratorConfig,
     DumpRepoGeneratorConfig,
 )
 from flymy_comfyui_repo_gen.scripts.schemas.inputs import YesOrNo
 
 app = Typer()
 
-
 BASE_PATH = pathlib.Path(__file__).parent
 PROJECT_PATH = pathlib.Path(__import__("flymy_comfyui_repo_gen").__file__).parent
 
 
-@app.command(name="generate_repository")
-def generate_repository_entry(
-    json_path: Annotated[pathlib.Path, typer.Option(prompt=True)],
-    output_dir: Annotated[pathlib.Path, typer.Option(prompt=True)],
-    repo_name: Annotated[str, typer.Option(prompt=True)],
-):
-    generate_repository(json_path, output_dir, repo_name)
-
-
-@app.command(name="generate_repository_json")
-def generate_repository_json(
-    workflow_api_json_path: Annotated[pathlib.Path, typer.Option(prompt=True)] = (
-        PROJECT_PATH.parent / "playground" / "workflow_api.json"
-    ),
-    output_path: Annotated[pathlib.Path, typer.Option(prompt=True)] = (
-        PROJECT_PATH.parent / "playground" / "resolution.json"
-    ),
+@app.command(name="process_workflow_api")
+def process_workflow_api(
+    workflow_api_json_path: Annotated[
+        pathlib.Path, typer.Option(prompt="Enter workflow_api.json path")
+    ] = (PROJECT_PATH.parent / "playground" / "workflow_api.json"),
+    output_path: Annotated[
+        pathlib.Path, typer.Option(prompt="Enter output json path")
+    ] = (PROJECT_PATH.parent / "playground" / "resolution.json"),
     skip_inputs: YesOrNo = YesOrNo.NO,
 ):
     if not workflow_api_json_path.parent.exists():
         raise ValueError(f"workflow_api.json path does not exist: {output_path}")
-    use_confirmation_mode = typer.prompt(
-        default=YesOrNo.NO,
-        show_default=True,
-        type=YesOrNo,
-        text="Should we ask about confirmation of a field input",
-    )
 
     default_prompt = lambda txt, input_type, input_default, val_proc, secure=False: typer.prompt(  # noqa
         text=txt,
-        confirmation_prompt=use_confirmation_mode,
+        confirmation_prompt=YesOrNo.NO,
         show_default=True,
         hide_input=secure,
         type=input_type,
@@ -68,14 +51,16 @@ def generate_repository_json(
     while not inputs_done and not skip_inputs:
         inputs = generate_resolution_inputs(workflow_api_json, default_prompt)
         inputs_done = typer.confirm(
-            f"Verify results: {json.dumps(inputs, indent=4)}",
+            f"\nVerify results: {json.dumps(inputs, indent=4)}",
             default=True,
             show_default=True,
         )
     repositories = generate_resolution_repositories(
         default_prompt,
         obtain_done_flag_callback=lambda: typer.confirm(
-            "Do you want to input any repository", default=False, show_default=True
+            "\nDoes your ComfyUI pipeline contain any repositories not previously specified?",
+            default=None,
+            show_default=True,
         ),
     )
     output_path.parent.mkdir(exist_ok=True, parents=True)
@@ -86,3 +71,21 @@ def generate_repository_json(
             comfy_repositories=repositories,
         ).model_dump_json(indent=4)
     )
+
+
+@app.command(name="generate_repository")
+def generate_repository_entry(
+    json_path: Annotated[
+        pathlib.Path,
+        typer.Option(
+            prompt="Enter config JSON path",
+        ),
+    ] = (PROJECT_PATH.parent / "playground" / "resolution.json"),
+    output_dir: Annotated[
+        pathlib.Path, typer.Option(prompt="Enter output directory")
+    ] = (PROJECT_PATH.parent / "playground" / "result"),
+    repo_name: Annotated[
+        str, typer.Option(prompt="Enter repository name")
+    ] = "my_comfyui_repo",
+):
+    generate_repository(json_path, output_dir, repo_name)
